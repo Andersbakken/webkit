Andersbakken/webkit

commit 3bf6fd09496f2471dcc92378291e31526390ecc1

Make WTF public headers use fully-qualified include paths and remove ForwardingHeaders/wtf https://bugs.webkit.org/show_bug.cgi?id=80363
Reviewed by Mark Rowe.

Source/JavaScriptCore:

Historically WTF has been part of JavaScriptCore, and on Mac and Windows
its headers have appeared as part of the "private" headers exported by
JavaScriptCore.  All of the WTF headers there are "flattened" into a single
private headers directory, and WebCore, WebKit and WebKit2 have used "ForwardingHeaders"
to re-map fully-qualified <wtf/text/Foo.h> includes to simple <JavaScriptCore/Foo.h> includes.

However, very soon, we are moving the WTF source code out of JavaScriptCore into its
own directory and project.  As part of such, the WTF headers will no longer be part of
the JavaScriptCore private interfaces.
In preparation for that, this change makes both the Mac and Win builds export
WTF headers in a non-flattened manner.  On Mac, that means into usr/local/include/wtf
(and subdirectories), on Windows for now that means JavaScriptCore/wtf (and subdirectories).

There are 5 parts to this change.
1.  Updates the JavaScriptCore XCode and VCProj files to actually install these headers
    (and header directories) into the appropriate places in the build directory.
2.  Updates JavaScriptCore.xcodeproj to look for these WTF headers in this install location
    (WebCore, WebKit, etc. had already been taught to look in previous patches).
3.  Fixes all JavaScriptCore source files, and WTF headers to include WTF headers
    using fully qualified paths.
4.  Stops the Mac and Win builds from installing these WTF headers in their old "flattened" location.
5.  Removes WebCore and WebKit ForwardingHeaders/wtf directories now that the flattened headers no longer exist.

Unfortunately we see no way to do this change in smaller parts, since all of these steps are interdependant.
It is possible there are internal Apple projects which depend on JavaScriptCore/Foo.h working for WTF

JavaScriptCore/JavaScriptCore.vcproj/JavaScriptCore/copy-files.cmd
@@ -47,15 +47,22 @@ for %%d in (
     parser
     profiler
     runtime
+    yarr
+) do (
+    xcopy /y /d ..\..\%%d\*.h "%PrivateHeadersDirectory%" >NUL
+)
+
+echo Copying WTF headers...
+for %%d in (
     wtf
     wtf\dtoa
     wtf\text
     wtf\threads
     wtf\unicode
     wtf\unicode\icu
-    yarr
 ) do (
-    xcopy /y /d ..\..\%%d\*.h "%PrivateHeadersDirectory%" >NUL
+    mkdir "%PrivateHeadersDirectory%\%%d" 2>NUL
+    xcopy /y /d ..\..\%%d\*.h "%PrivateHeadersDirectory%\%%d" >NUL
 )
 
 echo Copying resources...

commit 11073a15e9c077ecfc21ba0b0e9c88b6f875e3d5

Clean Windows build fails after r110033 https://bugs.webkit.org/show_bug.cgi?id=80553
Rubber-stamped by Jon Honeycutt and Eric Seidel.

Source/JavaScriptCore:

* JavaScriptCore.vcproj/JavaScriptCore/copy-files.cmd:
Place the implementation files next to their header files in the wtf/text subdirectory.
Use echo -F to tell xcopy that these are files (since there is apparently no flag).
* JavaScriptCore.vcproj/jsc/jsc.vcproj:
Update the path to those implementation files.
* JavaScriptCore.vcproj/testRegExp/testRegExp.vcproj:
Ditto.

Source/ThirdParty:

* gtest/msvc/gtest-md.vcproj:
Add the new JavaScriptCore include directory to the include path.

Source/WebCore:

* WebCore.vcproj/WebCore.vcproj:
Update the expected location of the copied JSC WTF string implementation files.

Tools:

Add the new JavaScriptCore include directory to some more vsprops files.

* DumpRenderTree/win/ImageDiffCommon.vsprops:
* WebKitTestRunner/Configurations/InjectedBundleCommon.vsprops:
* WebKitTestRunner/Configurations/WebKitTestRunnerCommon.vsprops:

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@110133 268f45cc-cd09-0410-ab3c-d52691b4dbfc

JavaScriptCore/JavaScriptCore.vcproj/JavaScriptCore/copy-files.cmd
@@ -77,7 +77,7 @@ for %%f in (
     wtf\text\StringImpl.cpp
     wtf\text\WTFString.cpp
 ) do (
-    xcopy /y /d ..\..\%%f "%PrivateHeadersDirectory%" >NUL
+    echo F | xcopy /y /d ..\..\%%f "%PrivateHeadersDirectory%\%%f" >NUL
 )
 
 goto :EOF

JavaScriptCore/JavaScriptCore.vcproj/jsc/jsc.vcproj
@@ -448,7 +448,7 @@
 	</References>
 	<Files>
 		<File
-			RelativePath="$(ConfigurationBuildDir)\include\private\JavaScriptCore\AtomicString.cpp"
+			RelativePath="$(ConfigurationBuildDir)\include\private\JavaScriptCore\wtf\text\AtomicString.cpp"
 			>
 		</File>
 		<File
@@ -456,15 +456,15 @@
 			>
 		</File>
 		<File
-			RelativePath="$(ConfigurationBuildDir)\include\private\JavaScriptCore\StringBuilder.cpp"
+			RelativePath="$(ConfigurationBuildDir)\include\private\JavaScriptCore\wtf\text\StringBuilder.cpp"
 			>
 		</File>
 		<File
-			RelativePath="$(ConfigurationBuildDir)\include\private\JavaScriptCore\StringImpl.cpp"
+			RelativePath="$(ConfigurationBuildDir)\include\private\JavaScriptCore\wtf\text\StringImpl.cpp"
 			>
 		</File>
 		<File
-			RelativePath="$(ConfigurationBuildDir)\include\private\JavaScriptCore\WTFString.cpp"
+			RelativePath="$(ConfigurationBuildDir)\include\private\JavaScriptCore\wtf\text\WTFString.cpp"
 			>
 		</File>
 	</Files>

JavaScriptCore/JavaScriptCore.vcproj/testRegExp/testRegExp.vcproj
@@ -448,15 +448,15 @@
 	</References>
 	<Files>
 		<File
-			RelativePath="$(ConfigurationBuildDir)\include\private\JavaScriptCore\AtomicString.cpp"
+			RelativePath="$(ConfigurationBuildDir)\include\private\JavaScriptCore\wtf\text\AtomicString.cpp"
 			>
 		</File>
 		<File
-			RelativePath="$(ConfigurationBuildDir)\include\private\JavaScriptCore\StringBuilder.cpp"
+			RelativePath="$(ConfigurationBuildDir)\include\private\JavaScriptCore\wtf\text\StringBuilder.cpp"
 			>
 		</File>
 		<File
-			RelativePath="$(ConfigurationBuildDir)\include\private\JavaScriptCore\StringImpl.cpp"
+			RelativePath="$(ConfigurationBuildDir)\include\private\JavaScriptCore\wtf\text\StringImpl.cpp"
 			>
 		</File>
 		<File
@@ -464,7 +464,7 @@
 			>
 		</File>
 		<File
-			RelativePath="$(ConfigurationBuildDir)\include\private\JavaScriptCore\WTFString.cpp"
+			RelativePath="$(ConfigurationBuildDir)\include\private\JavaScriptCore\wtf\text\WTFString.cpp"
 			>
 		</File>
 	</Files>